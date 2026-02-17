f3a7a9b9362b0e6312a3cdf421d8ab035f2e9e21
kozyrev197902-svg <kozyrev1979.02@gmail.com>
2026-02-17 12:27:11 +0700

    Create sheets.py

---
+++ sheets.py
@@ -0,0 +1,24 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from config import SPREADSHEET_ID
+
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name(
+    "credentials.json", scope
+)
+
+client = gspread.authorize(creds)
+spreadsheet = client.open_by_key(SPREADSHEET_ID)
+
+def get_list(col_letter):
+    sheet = spreadsheet.worksheet("Списки")
+    col = ord(col_letter.upper()) - 64
+    return [v for v in sheet.col_values(col)[1:] if v]
+
+def append_income(row):
+    sheet = spreadsheet.worksheet("Приходы")
+    sheet.append_row([""] + row)
